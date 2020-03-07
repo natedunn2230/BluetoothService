e98d7e7fec4a15d52d2b37564680e22d9979c2f0
natedunn2230 <ntd2230@gmail.com>
2020-03-07 15:47:44 -0500

    modified server to determine whether connection succeeded or not

--- bluetoothServer.py
+++ bluetoothServer.py
@@ -1,106 +1,125 @@
-
 import time
 import os
+import sys
+import subprocess
 from bluetooth import *
 import json
-
-
-def modify_wpa_sup(ssid, psk):
-	current_cred = '';
-
-	file_path = '/etc/wpa_supplicant/wpa_supplicant.conf'
-
-	with open(file_path, 'r') as f:
-		current_cred =  f.readlines()
-		f.close()
-
-	found_network = False
-	out_file = []
-	for line in current_cred:
-		current_line = line.strip()
-		if current_line.startswith('ssid'):
-			line = '\tssid="{}"\n'.format(ssid)
-		elif current_line.startswith('psk'):
-			line = '\tpsk="{}"\n'.format(psk)
-		elif current_line.startswith('network'):
-			found_network = True
-		
-		out_file.append(line)
-
-	if not found_network:
-		print('no network configurations found yet. Creating default network...')
-		
-		out_file = ['country=us\n', 'update_config=1\n', 'ctrl_interface=/var/run/wpa_supplicant\n\n', 
-		'network={\n', '\tssid="{}"\n\tpsk="{}"\n'.format(ssid, psk), '}\n']
-
-	with open(file_path, 'w') as f:
-		for line in out_file:
-			f.write(line)
-		f.close()
-
-	os.system("wpa_cli -i wlan0 reconfigure")
-
 
 ## Make device discoverable before setting up a server
 os.system("sudo hciconfig hci0 piscan")
 os.system("sudo hciconfig hci0 name 'Intellisert'")
 
+
+def very_connection():
+	iw_sub = subprocess.Popen(["iwconfig"], stdout=subprocess.PIPE)
+
+	iw_sub.wait()
+
+	(out, err) = iw_sub.communicate()
+	essid_idx = out.find("ESSID")
+	essid_idx += 6
+
+	return out[essid_idx] == '"'
+
+def modify_wpa_sup(ssid, psk, socket):
+	"""
+		Modifies the Wireless network's credentials in the wpa_supplicant file. The
+		service is then reconfigured to accept the new changes
+	"""
+	print('attempting to connect to network: {}'.format(ssid))
+
+	WPA_SUP_PATH = '/etc/wpa_supplicant/wpa_supplicant.conf'
+
+	## new information to be passed to the WPA_SUP file
+	out_file = ['country=us\n', 'update_config=1\n', 'ctrl_interface=/var/run/wpa_supplicant\n\n', 
+	'network={\n', '\tssid="{}"\n\tpsk="{}"\n'.format(ssid, psk), '}\n']
+
+	# write this information to the WPA_SUP file
+	with open(WPA_SUP_PATH, 'w') as f:
+		for line in out_file:
+			f.write(line)
+		f.close()
+	
+	# restart the wlan0 interface to connect to new network and block the rest of the program to wait on result
+	sub_proc = subprocess.Popen(["wpa_cli", "-i",  "wlan0", "reconfigure"], stdout=subprocess.PIPE)
+	sub_proc.wait()
+	time.sleep(10)
+	
+	(out, err) = sub_proc.communicate()
+
+
+        if very_connection() and out == 'OK\n':
+		return True
+	else:
+		return False
+
+
 while True:
-    server_sock=BluetoothSocket( RFCOMM )
-    server_sock.bind(("",PORT_ANY))
-    server_sock.listen(1)
+	try:
+		server_sock=BluetoothSocket( RFCOMM )
+		server_sock.bind(("",PORT_ANY))
+		server_sock.listen(1)
 
-    port = server_sock.getsockname()[1]
+		port = server_sock.getsockname()[1]
 
-    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+		uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-    advertise_service( server_sock, "AquaPiServer",
-                    service_id = uuid,
-                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                    profiles = [ SERIAL_PORT_PROFILE ])
-    print("waiting for connection....")
+		advertise_service( server_sock, "AquaPiServer",
+						service_id = uuid,
+						service_classes = [ uuid, SERIAL_PORT_CLASS ],
+						profiles = [ SERIAL_PORT_PROFILE ])
+		print("waiting for connection....")
 
-    client_sock, client_info = server_sock.accept()
-    print("Accepted connection from", client_info)
-    try:
-        while True:
-                        
-            # response back to client will be in format {"success": bool, "msg": string}
-            response = '{"success": true, "message": "successful config"}'
-            
-            ssid = '';
-            password = '';
+		client_sock, client_info = server_sock.accept()
+		print("Accepted connection from", client_info)
 
-            data = client_sock.recv(1024)
-            if not data:
-                break
-            
-            #data will be in format {"ssid": string, "key": string}
-            jsonData  = json.loads(data)
-                        
-            # parse data keys
-            if 'ssid' and 'key' in jsonData:
-                ssid = jsonData['ssid']
-                password = jsonData['key']
+		while True:
+						
+			# response back to client will be in format {"success": bool, "msg": string}
+			response = ''
+			
+			ssid = '';
+			password = '';
+
+			data = client_sock.recv(1024)
+			if not data:
+				break
+			
+			#data will be in format {"ssid": string, "key": string}
+			jsonData  = json.loads(data)
+						
+			# parse data keys
+			try:
+				ssid = jsonData['ssid']
+				password = jsonData['key']
 				
-                ## modify the wpa_supplicant file to hold new network changes
-                modify_wpa_sup(ssid, password)
-            else:
-                response = '{"success": false, "message": "parameters ssid and key required"}'
+				## modify the wpa_supplicant file to hold new network changes
+				wpa_result = modify_wpa_sup(ssid, password, client_sock)
+				
+				if not wpa_result:
+                                        print('Could not connect to network')
+					response = '{"success": false, "message": "could not connect to specified network"}'
+				else:
+                                        print('Connected to network')
+					response = '{"success": true, "message": "successfully connected to network"}'
 					
-        client_sock.send(response)
-            
-    except OSError as e:
-        print(e)
-        pass
-    except BluetoothError as e:
-        print(e)
-        pass
+			except KeyError as e:
+				response = '{"success": false, "message": "parameters ssid and key required"}'
 
-    print("disconnected")
+			client_sock.send(response)
+	except BluetoothError as e:
+		print(e)
+	except OSError as e:
+		print(e)
+	except Exception as e:
+		print(e)
+	except KeyboardInterrupt as e:
+		try:
+			sys.exit(0)
+		except SystemExit:
+			os._exit(0)
 
-    client_sock.close()
-    server_sock.close()
+	print("disconnected")
 
-
-
+	client_sock.close()
+	server_sock.close()
